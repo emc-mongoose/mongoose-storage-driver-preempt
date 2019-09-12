02c50aad3f9bf5cc24316bb9dccd8346ae405ae8
kurila <andrey.kurilov@emc.com>
2019-09-12 16:39:29 +0300

    v4.2.20

--- src/main/java/com/emc/mongoose/storage/driver/preempt/WorkerTask.java
+++ src/main/java/com/emc/mongoose/storage/driver/preempt/WorkerTask.java
@@ -1,6 +1,8 @@
 package com.emc.mongoose.storage.driver.preempt;
 
+import com.emc.mongoose.base.logging.LogUtil;
 import com.github.akurilov.commons.concurrent.AsyncRunnable.State;
+import org.apache.logging.log4j.Level;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import static com.emc.mongoose.base.Exceptions.throwUncheckedIfInterrupted;
 import static com.github.akurilov.commons.concurrent.AsyncRunnable.State.SHUTDOWN;
 import static com.github.akurilov.commons.concurrent.AsyncRunnable.State.STARTED;
 
@@ -34,22 +37,27 @@ implements Runnable {
     @Override
     public final void run() {
         final var ops = new ArrayList<T>(batchSize);
-        while(true) {
-            final var state = stateSupplier.get();
-            final var n = inQueue.drainTo(ops, batchSize);
-            if(SHUTDOWN.equals(state)) {
-                if(0 == n) {
-                    break; // shut down and nothing to do more, exit the loop
+        try {
+            while(true) {
+                final var state = stateSupplier.get();
+                final var n = inQueue.drainTo(ops, batchSize);
+                if(SHUTDOWN.equals(state)) {
+                    if(0 == n) {
+                        break; // shut down and nothing to do more, exit the loop
+                    }
+                } else if(!STARTED.equals(state)) {
+                    break; // not shut down and not started, exit the loop
                 }
-            } else if(!STARTED.equals(state)) {
-                break; // not shut down and not started, exit the loop
+                if(1 == n) {
+                    action.accept(ops.get(0));
+                } else if(n > 1) {
+                    batchAction.accept(ops.subList(0, n));
+                }
+                ops.clear();
             }
-            if(1 == n) {
-                action.accept(ops.get(0));
-            } else if(n > 1) {
-                batchAction.accept(ops.subList(0, n));
-            }
-            ops.clear();
+        } catch (final Throwable e) {
+            throwUncheckedIfInterrupted(e);
+            LogUtil.exception(Level.WARN, e, "Unexpected worker failure");
         }
     }
 }
