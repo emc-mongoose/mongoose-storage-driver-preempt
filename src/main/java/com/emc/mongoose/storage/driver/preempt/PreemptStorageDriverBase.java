505d3c982bf0d45f0a094b6aaba3bfcb4ec2fe15
kurila <andrey.kurilov@emc.com>
2019-09-12 11:59:00 +0300

    v4.2.20

--- src/main/java/com/emc/mongoose/storage/driver/preempt/PreemptStorageDriverBase.java
+++ src/main/java/com/emc/mongoose/storage/driver/preempt/PreemptStorageDriverBase.java
@@ -6,7 +6,6 @@ import com.emc.mongoose.base.data.DataInput;
 import com.emc.mongoose.base.config.IllegalConfigurationException;
 import com.emc.mongoose.base.item.Item;
 import com.emc.mongoose.base.item.op.Operation;
-import com.emc.mongoose.base.item.op.Operation.Status;
 import com.emc.mongoose.base.logging.Loggers;
 import com.emc.mongoose.base.storage.driver.StorageDriver;
 import com.emc.mongoose.base.storage.driver.StorageDriverBase;
@@ -16,18 +15,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
 
 public abstract class PreemptStorageDriverBase<I extends Item, O extends Operation<I>>
 				extends StorageDriverBase<I, O> implements StorageDriver<I, O> {
 
 	public static final int BATCH_MODE_INPUT_OP_COUNT_LIMIT = 1_000_000;
 
-	private final ThreadPoolExecutor ioExecutor;
-	private final BlockingQueue<Runnable> incomingOps;
+	private final BlockingQueue<O> incomingOps;
+	private final List<Thread> ioWorkers;
+	private final LongAdder scheduledOpCount = new LongAdder();
+	private final LongAdder completedOpCount = new LongAdder();
 
 	protected abstract ThreadFactory ioWorkerThreadFactory();
 
@@ -48,13 +48,15 @@ public abstract class PreemptStorageDriverBase<I extends Item, O extends Operati
 			);
 		}
 		incomingOps = new ArrayBlockingQueue<>(inQueueSize);
-		ioExecutor = new ThreadPoolExecutor(
-						ioWorkerCount,
-						ioWorkerCount,
-						0,
-						TimeUnit.SECONDS,
-						incomingOps,
-						ioWorkerThreadFactory());
+		ioWorkers = new ArrayList<>(ioWorkerCount);
+		final var ioWorkerThreadFactory = ioWorkerThreadFactory();
+		for(var i = 0; i < ioWorkerCount; i ++) {
+			final var ioWorkerTask = new WorkerTask<>(
+				batchSize, incomingOps, this::execute, this::execute, this::state
+			);
+			final var ioWorker = ioWorkerThreadFactory.newThread(ioWorkerTask);
+			ioWorkers.add(ioWorker);
+		}
 	}
 
 	@Override
@@ -62,7 +64,11 @@ public abstract class PreemptStorageDriverBase<I extends Item, O extends Operati
 		if(!isStarted()) {
 			throwUnchecked(new EOFException());
 		}
-		return incomingOps.offer(wrapToBlocking(op));
+		final var submitted = incomingOps.offer(op);
+		if(submitted) {
+			scheduledOpCount.increment();
+		}
+		return submitted;
 	}
 
 	@Override
@@ -71,45 +77,18 @@ public abstract class PreemptStorageDriverBase<I extends Item, O extends Operati
 			throwUnchecked(new EOFException());
 		}
 		int i = from;
-		if(isBatch(ops, from, to)) {
-			if(incomingOps.offer(wrapToBlocking(ops, from, to))) {
-				i = to;
-			}
-		} else {
-			while(i < to) {
-				incomingOps.offer(wrapToBlocking(ops.get(i)));
-				i ++;
-			}
+		while(i < to && incomingOps.offer(ops.get(i))) {
+			i ++;
 		}
-		return i - from;
+		final var n = i - from;
+		scheduledOpCount.add(n);
+		return n;
 	}
 
 	@Override
 	public final int put(final List<O> ops)  {
 		return put(ops, 0, ops.size());
 	}
-
-	private Runnable wrapToBlocking(final O op)  {
-		if (prepare(op)) {
-			return () -> execute(op);
-		} else {
-			return () -> op.status(Status.FAIL_UNKNOWN);
-		}
-	}
-
-	private Runnable wrapToBlocking(final List<O> ops, final int from, final int to) {
-		// should copy the ops into the other buffer as far as invoker will clean the source buffer after put(...) exit
-		final var opsRangeCopy = new ArrayList<O>();
-		O op;
-		for(var i = from; i < to; i ++) {
-			op = ops.get(i);
-			prepare(op);
-			opsRangeCopy.add(op);
-		}
-		return () -> execute(opsRangeCopy);
-	}
-
-	protected abstract boolean isBatch(final List<O> ops, final int from, final int to);
 
 	/**
 	 * Should invoke or schedule handleCompleted call
@@ -125,34 +104,32 @@ public abstract class PreemptStorageDriverBase<I extends Item, O extends Operati
 
 	@Override
 	public final int activeOpCount() {
-		return ioExecutor.getActiveCount();
+		return (int) (scheduledOpCount() - completedOpCount() - incomingOps.size());
 	}
 
 	@Override
 	public final long scheduledOpCount() {
-		return ioExecutor.getTaskCount();
+		return scheduledOpCount.sum();
 	}
 
 	@Override
 	public final long completedOpCount() {
-		return ioExecutor.getCompletedTaskCount();
+		return completedOpCount.sum();
 	}
 
 	@Override
 	public final boolean isIdle() {
-		return ioExecutor.getActiveCount() == 0;
+		return activeOpCount() == 0;
 	}
 
 	@Override
 	protected void doStart() {
-		ioExecutor.prestartAllCoreThreads();
+		ioWorkers.forEach(Thread::start);
 		Loggers.MSG.debug("{}: started", toString());
 	}
 
 	@Override
 	protected void doShutdown() {
-		// prevent enqueuing new load operations
-		ioExecutor.shutdown();
 		Loggers.MSG.debug("{}: shut down", toString());
 	}
 
@@ -160,22 +137,22 @@ public abstract class PreemptStorageDriverBase<I extends Item, O extends Operati
 	protected void doStop()  {
 		Loggers.MSG.debug("{}: interrupting...", toString());
 		incomingOps.clear(); // drop all internally pending load operations
-		try {
-			if (ioExecutor.awaitTermination(1, TimeUnit.SECONDS)) {
-				Loggers.MSG.debug("{}: interrupting finished in 1 seconds", toString());
-			} else {
-				Loggers.ERR.debug("{}: interrupting did not finish in 1 second, forcing", toString());
-			}
-		} catch (final InterruptedException e) {
-			ioExecutor.shutdownNow();
-			throwUnchecked(e);
-		} finally {
-			Loggers.MSG.debug("{}: interrupted", toString());
-		}
+		ioWorkers.forEach(Thread::interrupt);
 	}
 
 	@Override
 	public boolean await(final long timeout, final TimeUnit timeUnit) throws InterruptedException {
-		return ioExecutor.awaitTermination(timeout, timeUnit);
+		return ioWorkers
+			.parallelStream()
+			.noneMatch(
+				ioWorker -> {
+					try {
+						ioWorker.join(timeUnit.toMillis(timeout));
+					} catch (final InterruptedException e) {
+						throwUnchecked(e);
+					}
+					return ioWorker.isAlive();
+				}
+			);
 	}
 }
---
+++ src/main/java/com/emc/mongoose/storage/driver/preempt/WorkerTask.java
@@ -0,0 +1,55 @@
+package com.emc.mongoose.storage.driver.preempt;
+
+import com.github.akurilov.commons.concurrent.AsyncRunnable.State;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static com.github.akurilov.commons.concurrent.AsyncRunnable.State.SHUTDOWN;
+import static com.github.akurilov.commons.concurrent.AsyncRunnable.State.STARTED;
+
+public class WorkerTask<T>
+implements Runnable {
+
+    private final int batchSize;
+    private final BlockingQueue<T> inQueue;
+    private final Consumer<T> action;
+    private final Consumer<List<T>> batchAction;
+    private final Supplier<State> stateSupplier;
+
+    public WorkerTask(
+        final int batchSize, final BlockingQueue<T> inQueue, final Consumer<T> action,
+        final Consumer<List<T>> batchAction, final Supplier<State> stateSupplier
+    ) {
+        this.batchSize = batchSize;
+        this.inQueue = inQueue;
+        this.action = action;
+        this.batchAction = batchAction;
+        this.stateSupplier = stateSupplier;
+    }
+
+    @Override
+    public final void run() {
+        final var ops = new ArrayList<T>(batchSize);
+        while(true) {
+            final var state = stateSupplier.get();
+            final var n = inQueue.drainTo(ops, batchSize);
+            if(SHUTDOWN.equals(state)) {
+                if(0 == n) {
+                    break; // shut down and nothing to do more, exit the loop
+                }
+            } else if(!STARTED.equals(state)) {
+                break; // not shut down and not started, exit the loop
+            }
+            if(1 == n) {
+                action.accept(ops.get(0));
+            } else if(n > 1) {
+                batchAction.accept(ops.subList(0, n));
+            }
+            ops.clear();
+        }
+    }
+}
